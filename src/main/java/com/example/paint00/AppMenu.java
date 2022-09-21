cebbf0aa470a62293f9ab30fd061cfb1519353ba
aashakya <67236701+aashakya@users.noreply.github.com>
2022-09-21 09:47:26 -0500

    Separate Menu into new class, set stage to fullscreen

---
+++ src/main/java/com/example/paint00/AppMenu.java
@@ -0,0 +1,129 @@
+package com.example.paint00;
+
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class AppMenu{
+    //create menu bar
+    MenuBar menuBar = new MenuBar();
+    VBox vbox = new VBox();
+    final String[] nameOfFile = new String[1]; // to save file
+    final String[] ext = new String[1];
+
+    AppMenu(Stage primaryStage, VBox layout, GraphicsContext gc, GridPane gridPane, Canvas canvas){
+        //create menu
+        Menu fileMenu = new Menu("File");
+        Menu helpMenu = new Menu("Help");
+        
+        //crete menu items
+        // For File
+        MenuItem saveItem = new MenuItem("Save");
+        MenuItem saveAsItem = new MenuItem("Save As");
+        MenuItem openItem = new MenuItem("Open Image");
+        MenuItem closeApp = new MenuItem("Close");
+        
+        // For Help
+        MenuItem helpItem = new MenuItem("Help");
+        MenuItem aboutItem= new MenuItem("About");
+        //add menu item to menu
+        //for file menu items
+        fileMenu.getItems().addAll(saveItem,saveAsItem,openItem,closeApp);
+        //for help menu items
+        helpMenu.getItems().addAll(helpItem,aboutItem);
+
+        //add menu to the menu bar
+        menuBar.getMenus().addAll(fileMenu,helpMenu);
+        layout.getChildren().add(menuBar);
+        ImageView imageInserted = new ImageView();
+        // for opening the file
+        FileChooser fileChooser = new FileChooser();
+        openItem.setOnAction(e -> {
+            fileChooser.setTitle("Open File");
+            fileChooser.getExtensionFilters().addAll(              //Including filters for extension types
+                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif", "*.jpeg"),
+                    new FileChooser.ExtensionFilter("All Files", "*.*")
+            );
+            File insImg = fileChooser.showOpenDialog(primaryStage);
+            if (insImg != null) {
+                try {
+                    InputStream io = new FileInputStream(insImg);
+                    Image img = new Image(io);
+
+                    imageInserted.setImage(img);        //Specifying placement & sizing of selected image
+                    imageInserted.setFitWidth(img.getWidth());
+                    imageInserted.setFitHeight(img.getHeight());
+                    imageInserted.setPreserveRatio(true);
+
+                    gc.getCanvas().setWidth(img.getWidth());
+                    gc.getCanvas().setHeight(img.getHeight());
+                    gc.drawImage(img, 0,0, img.getWidth(),img.getHeight());
+                    gridPane.add(canvas,0,0);
+
+                }
+                catch (IOException ex) {
+                    System.out.println("Error");
+                }
+            }
+        });
+        saveAsItem.setOnAction(e -> {
+            FileChooser saveImgAs = new FileChooser();
+            saveImgAs.setTitle("Save image as");
+            saveImgAs.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("PNG file","*.png"),
+                    new FileChooser.ExtensionFilter("PDF file","*.pdf"),
+                    new FileChooser.ExtensionFilter("BMP file","*.bmp"));
+            File savedImg = saveImgAs.showSaveDialog(null);
+            nameOfFile[0] = savedImg.getName();
+            //int index = savedImg.getName().indexOf(".");
+            //if (index != -1){
+            ///.substring(0,index);}
+            ext[0] = nameOfFile[0].substring(1 + nameOfFile[0].lastIndexOf(".")).toLowerCase();
+            try {           //attempt to make a save file from the inserted image
+                WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+                canvas.snapshot(null,writableImage);
+                ImageIO.write(SwingFXUtils.fromFXImage(writableImage,null), ext[0], savedImg);
+            } catch (IOException o) {   //If the above line breaks, throw an exception
+                throw new RuntimeException(o);
+            }
+        });
+//        saveItem.setOnAction(e -> {
+//            try {           //attempt to make a save file from the inserted image
+//                WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+//                canvas.snapshot(null,writableImage);
+//                RenderedImage ri = SwingFXUtils.fromFXImage(imageInserted.getImage(),null);
+//                File savedImg = null;
+        //        ImageIO.write(SwingFXUtils.fromFXImage(writableImage,null), ext[0], savedImg);
+//                ImageIO.write(ri, ext[0], savedImg);
+//                System.out.println(new File(nameOfFile[0]));
+//
+//            } catch (IOException o) {   //If the above line breaks, throw an exception
+//                System.out.println("Save As first");
+//            }
+//        });
+        closeApp.setOnAction(e-> Platform.exit());
+
+
+    }
+
+
+}
--- src/main/java/com/example/paint00/Main.java
+++ src/main/java/com/example/paint00/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
@@ -15,125 +16,39 @@ import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import javax.imageio.ImageIO;
 import javafx.scene.input.MouseEvent;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws IOException {
+    public void start(Stage primaryStage) {
         //set up the title for the stage
         primaryStage.setTitle("Paint 00");
+        // Get primary screen bounds
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        primaryStage.setX(screenBounds.getMinX());
+        primaryStage.setY(screenBounds.getMinY());
+        VBox layout = new VBox(7);// create a vbox with 7 spacing between each child
 
         int canvHeight = 1080;
         int canvWidth = 1080;
 
         Canvas canvas= new Canvas(canvHeight,canvWidth); // create the canvas to paint on
-        final GraphicsContext gc = canvas.getGraphicsContext2D();
+        GraphicsContext gc = canvas.getGraphicsContext2D();
         initDraw(gc, canvWidth,canvHeight);
         ScrollPane sp = new ScrollPane();
         ColorPicker colorPicker = new ColorPicker();
-        VBox layout = new VBox();// create a vbox with 7 spacing between each child
+        colorPicker.setValue(Color.BLACK);
 
-        final String[] nameOfFile = new String[1]; // to save file
-        final String[] ext = new String[1];
+
         //create a grid pane & border pane
         GridPane gridPane = new GridPane();
         gridPane.add(canvas,0,0);
 
-        //create the menu
-        Menu fileMenu = new Menu("File");
-
-        ImageView imageInserted = new ImageView();
-
-        //crete menu items
-        //MenuItem saveItem = new MenuItem("Save");
-        MenuItem saveAsItem = new MenuItem("Save As");
-        MenuItem openItem = new MenuItem("Open Image");
-        MenuItem closeApp = new MenuItem("Close");
-
-        //add menu item to menu
-        //fileMenu.getItems().add(saveItem);
-        fileMenu.getItems().add(saveAsItem);
-        fileMenu.getItems().add(openItem);
-        fileMenu.getItems().add(closeApp);
-
-        //create a menu bar
-        MenuBar topBar = new MenuBar();
-
-        //add menu to the menu bar
-        topBar.getMenus().add(fileMenu);
-
-        // for opening the file
-        FileChooser fileChooser = new FileChooser();
-        openItem.setOnAction(e -> {
-            fileChooser.setTitle("Open File");
-            fileChooser.getExtensionFilters().addAll(              //Including filters for extension types
-                    new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif", "*.jpeg"),
-                    new ExtensionFilter("All Files", "*.*")
-            );
-            File insImg = fileChooser.showOpenDialog(primaryStage);
-            if (insImg != null) {
-                try {
-                    InputStream io = new FileInputStream(insImg);
-                    Image img = new Image(io);
-
-                    imageInserted.setImage(img);        //Specifying placement & sizing of selected image
-                    imageInserted.setFitWidth(img.getWidth());
-                    imageInserted.setFitHeight(img.getHeight());
-                    imageInserted.setPreserveRatio(true);
-
-                    gc.getCanvas().setWidth(img.getWidth());
-                    gc.getCanvas().setHeight(img.getHeight());
-                    gc.drawImage(img, 0,0, img.getWidth(),img.getHeight());
-                    gridPane.add(canvas,0,0);
-
-                }
-                catch (IOException ex) {
-                    System.out.println("Error");
-                }
-            }
-        });
-        saveAsItem.setOnAction(e -> {
-            FileChooser saveImgAs = new FileChooser();
-            saveImgAs.setTitle("Save image as");
-            saveImgAs.getExtensionFilters().addAll(
-                    new ExtensionFilter("PNG file","*.png"),
-                    new ExtensionFilter("PDF file","*.pdf"),
-                    new ExtensionFilter("BMP file","*.bmp"));
-            File savedImg = saveImgAs.showSaveDialog(null);
-            nameOfFile[0] = savedImg.getName();
-            //int index = savedImg.getName().indexOf(".");
-            //if (index != -1){
-            ///.substring(0,index);}
-            ext[0] = nameOfFile[0].substring(1 + nameOfFile[0].lastIndexOf(".")).toLowerCase();
-            try {           //attempt to make a save file from the inserted image
-                WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
-                canvas.snapshot(null,writableImage);
-                ImageIO.write(SwingFXUtils.fromFXImage(writableImage,null), ext[0], savedImg);
-            } catch (IOException o) {   //If the above line breaks, throw an exception
-                throw new RuntimeException(o);
-            }
-        });
-//        saveItem.setOnAction(e -> {
-//            try {           //attempt to make a save file from the inserted image
-//                WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
-//                canvas.snapshot(null,writableImage);
-//                RenderedImage ri = SwingFXUtils.fromFXImage(imageInserted.getImage(),null);
-//                File savedImg = null;
-//                ImageIO.write(ri, nameOfFile[0], (ImageOutputStream) ri);
-//                System.out.println(new File(nameOfFile[0]));
-//
-//            } catch (IOException o) {   //If the above line breaks, throw an exception
-//                System.out.println("Save As first");
-//            }
-//        });
-        closeApp.setOnAction(e-> Platform.exit());
 
         HBox toolBoxTop = new HBox();
         toolBoxTop.setPadding(new Insets(15, 12, 15, 12));
@@ -171,13 +86,15 @@ public class Main extends Application {
         canvas.addEventHandler(MouseEvent.MOUSE_RELEASED, event -> {
 
         });
+        // Menu bar
+        AppMenu topMenu = new AppMenu(primaryStage, layout, gc, gridPane,canvas);
 
-        layout.getChildren().add(topBar);
         layout.getChildren().add(toolBoxTop);
         layout.getChildren().add(gridPane);
         sp.setContent(gridPane);
         layout.getChildren().add(sp);
-        Scene scene = new Scene(layout, 1000,1000);
+
+        Scene scene = new Scene(layout, 10000,1000);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
