9fc841542e4e8a5820dbf3222811c2ee1e3e258d
aashakya <67236701+aashakya@users.noreply.github.com>
2022-09-23 09:40:17 -0500

    Fix open file execution error

--- src/main/java/com/example/paint00/AppMenu.java
+++ src/main/java/com/example/paint00/AppMenu.java
@@ -8,28 +8,24 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-
 import javax.imageio.ImageIO;
 import java.awt.image.RenderedImage;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class AppMenu{
     //create menu bar
     MenuBar menuBar = new MenuBar();
     VBox vbox = new VBox();
-    final String[] nameOfFile = new String[1]; // to save file
-    final String[] ext = new String[1];
+    String nameOfFile = "Untitled"; // to save file
+    String[] ext = new String[1];
+    File saveFile;
 
-    AppMenu(Stage primaryStage, GraphicsContext gc, GridPane gridPane, Canvas canvas){
+    AppMenu(Stage primaryStage, GraphicsContext gc, Canvas canvas){
         //create menu
         Menu fileMenu = new Menu("File");
         Menu helpMenu = new Menu("Help");
@@ -53,35 +49,22 @@ public class AppMenu{
         //add menu to the menu bar
         menuBar.getMenus().addAll(fileMenu,helpMenu);
         vbox.getChildren().add(menuBar);
-        ImageView imageInserted = new ImageView();
         // for opening the file
         FileChooser fileChooser = new FileChooser();
         openItem.setOnAction(e -> {
             fileChooser.setTitle("Open File");
             fileChooser.getExtensionFilters().addAll(              //Including filters for extension types
-                    new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif", "*.jpeg"),
-                    new FileChooser.ExtensionFilter("All Files", "*.*")
+                    new FileChooser.ExtensionFilter("Image Files", "*.png"),
+                    new FileChooser.ExtensionFilter("JPEG File", "*.jpeg"),
+                    new FileChooser.ExtensionFilter("PDF File", "*.pdf")
             );
             File insImg = fileChooser.showOpenDialog(primaryStage);
             if (insImg != null) {
-                try {
-                    InputStream io = new FileInputStream(insImg);
-                    Image img = new Image(io);
-
-                    imageInserted.setImage(img);        //Specifying placement & sizing of selected image
-                    imageInserted.setFitWidth(img.getWidth());
-                    imageInserted.setFitHeight(img.getHeight());
-                    imageInserted.setPreserveRatio(true);
-
-                    gc.getCanvas().setWidth(img.getWidth());
-                    gc.getCanvas().setHeight(img.getHeight());
-                    gc.drawImage(img, 0,0, img.getWidth(),img.getHeight());
-                    gridPane.add(canvas,0,0);
-
-                }
-                catch (IOException ex) {
-                    System.out.println("Error");
-                }
+                Image img = new Image(insImg.toURI().toString());
+                saveFile = insImg;
+                canvas.setWidth(img.getWidth());
+                canvas.setHeight(img.getHeight());
+                gc.drawImage(img, 0,0);
             }
         });
         saveAsItem.setOnAction(e -> {
@@ -92,11 +75,8 @@ public class AppMenu{
                     new FileChooser.ExtensionFilter("PDF file","*.pdf"),
                     new FileChooser.ExtensionFilter("BMP file","*.bmp"));
             File savedImg = saveImgAs.showSaveDialog(null);
-            nameOfFile[0] = savedImg.getName();
-            //int index = savedImg.getName().indexOf(".");
-            //if (index != -1){
-            ///.substring(0,index);}
-            ext[0] = nameOfFile[0].substring(1 + nameOfFile[0].lastIndexOf(".")).toLowerCase();
+            nameOfFile = savedImg.getName();
+            ext[0] = nameOfFile.substring(1 + nameOfFile.lastIndexOf(".")).toLowerCase();
             try {           //attempt to make a save file from the inserted image
                 WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
                 canvas.snapshot(null,writableImage);
@@ -104,17 +84,15 @@ public class AppMenu{
             } catch (IOException o) {   //If the above line breaks, throw an exception
                 throw new RuntimeException(o);
             }
+            saveFile = savedImg;
         });
         saveItem.setOnAction(e -> {
             try {           //attempt to make a save file from the inserted image
-                WritableImage writableImage;
-                writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+                WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
                 canvas.snapshot(null,writableImage);
-                RenderedImage ri = SwingFXUtils.fromFXImage(imageInserted.getImage(),null);
-                File savedImg = null;
-                ImageIO.write(SwingFXUtils.fromFXImage(writableImage,null), ext[0], savedImg);
-                ImageIO.write(ri, ext[0], savedImg);
-                System.out.println(new File(nameOfFile[0]));
+                RenderedImage ri = SwingFXUtils.fromFXImage(writableImage,null);
+                ImageIO.write(ri, "png", saveFile);
+                System.out.println(new File(nameOfFile));
 
             } catch (IOException o) {   //If the above line breaks, throw an exception
                 System.out.println("Save As first");
