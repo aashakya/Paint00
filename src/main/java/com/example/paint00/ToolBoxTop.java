a29cf1b640fcaa99a2843999f17ab2c02e3c868d
aashakya <67236701+aashakya@users.noreply.github.com>
2022-09-23 10:40:49 -0500

    Add Shapes and width resize to the toolbar

--- src/main/java/com/example/paint00/ToolBoxTop.java
+++ src/main/java/com/example/paint00/ToolBoxTop.java
@@ -2,9 +2,7 @@ package com.example.paint00;
 
 import javafx.geometry.Insets;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.ColorPicker;
-import javafx.scene.control.Slider;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -12,29 +10,41 @@ import javafx.scene.canvas.Canvas;
 
 public class ToolBoxTop {
     ColorPicker colorPicker = new ColorPicker();
+    Slider brushSize = new Slider(1,100, 10);
+
+    ToggleButton pen = new ToggleButton("Pen");
+    ToggleButton eraser = new ToggleButton("Eraser");
+    ToggleButton drawLine = new ToggleButton("Draw Line");
+    ToggleButton drawSquare = new ToggleButton("Draw Square");
+    ToggleButton drawCircle = new ToggleButton("Draw Circle");
+    ToggleButton drawRect = new ToggleButton("Draw Rectangle");
+    ToggleButton drawEllipse = new ToggleButton("Draw ellipse");
+
+    ToggleGroup toggleGroup = new ToggleGroup();
+    TextField canvasWidth = new TextField();
+
     HBox toolBoxTop = new HBox();
     ToolBoxTop(Canvas canvas, GraphicsContext gc){
+        pen.setSelected(true);
+        toggleGroup.getToggles().addAll(pen,eraser,drawLine,drawSquare,drawCircle,drawRect,drawEllipse);
         colorPicker.setValue(Color.BLACK);
 
         toolBoxTop.setPadding(new Insets(15, 12, 15, 12));
         toolBoxTop.setSpacing(10);
-        Slider brushSize = new Slider(1,100, 10);
 
         brushSize.setShowTickLabels(true);
         brushSize.setMajorTickUnit(10f);
         brushSize.setBlockIncrement(10f);
-        ToggleButton eraser = new ToggleButton("Eraser");
 
-        toolBoxTop.getChildren().add(0, colorPicker);
-        toolBoxTop.getChildren().add(1,brushSize);
-        toolBoxTop.getChildren().add(2,eraser);
+        canvasWidth.setPromptText("Enter width");
+        toolBoxTop.getChildren().addAll(colorPicker,brushSize,canvasWidth,pen,eraser,drawLine,drawSquare,drawCircle,drawRect,drawEllipse);
 
         canvas.addEventHandler(MouseEvent.MOUSE_PRESSED, event -> {
             gc.setLineWidth(brushSize.getValue());
             if (eraser.isSelected()){
                 gc.setStroke(Color.WHITE);
             }
-            else{
+            else if (pen.isSelected()){
                 gc.setStroke(colorPicker.getValue());
             }
             gc.beginPath();
